ce92b9466285febb6619dce31fdc8dc293cbee2c
Gam5510 <gameruz673@gmail.com>
2026-02-15 18:56:00 +0500

    Update comment.py

--- itdpy/models/comment.py
+++ itdpy/models/comment.py
@@ -1,5 +1,6 @@
-﻿from __future__ import annotations
+from __future__ import annotations
 
+from typing import Optional
 from pydantic import AliasChoices, Field
 
 from .attachment import Attachment
@@ -11,12 +12,23 @@ class Comment(ITDBaseModel):
     id: str
     content: str | None = None
 
-    likes_count: int = Field(0, alias="likesCount", validation_alias=AliasChoices("likesCount", "likkesCount"))
+    likes_count: int = Field(
+        0,
+        alias="likesCount",
+        validation_alias=AliasChoices("likesCount", "likkesCount"),
+    )
+
     replies_count: int = Field(0, alias="repliesCount")
     is_liked: bool = Field(False, alias="isLiked")
 
     created_at: str = Field(..., alias="createdAt")
 
-    author: UserLite | None = None
+    author: Optional[UserLite] = None
     attachments: list[Attachment] = Field(default_factory=list)
-    replies: list[Comment] = Field(default_factory=list)
+
+    reply_to: Optional[UserLite] = Field(None, alias="replyTo")
+
+    replies: list["Comment"] = Field(default_factory=list)
+
+    def __repr__(self) -> str:
+        return f"<Comment {self.id}>"
